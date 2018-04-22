9e2ce5d51996f156bbd3e65eb311c585f11f1abb
JordanBDixon <jordan.dixon@colorado.edu>
2018-04-22 17:15:16 -0600

    Introduction of HoloToolkit
    Began on Main_current that functioned on Hololens without speech commands working, no Vuforia. Installed the HoloToolkit and ensured identical functionality on HoloLens. Added in first basic speech command (on EVA Clock) via Unity settings + HoloToolkit functions (Speech Input Source, Speech Input Handler, Input Manager, SetG Global Listener) + custom script (HUDKeywords2).
    **Still includes KeywordManager that did not work with Vuforia, but works fine here. Can be removed - does not add functionality

---
+++ Assets/Scripts/HUDKeywords2.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Text;
+
+namespace HoloToolkit.Unity.InputModule.Tests
+{
+    public class HUDKeywords2 : MonoBehaviour, ISpeechHandler
+    {
+
+        public void ToggleObject(string command)
+        {
+            switch (command)
+            {
+                case "Basic on":
+	                  GameObject.Find ("EVA Clock").GetComponent<Text> ().enabled = true;
+                    break;
+                case "Basic off":
+                    GameObject.Find ("EVA Clock").GetComponent<Text> ().enabled = false;
+                    break;
+            }
+        }
+
+        public void OnSpeechKeywordRecognized(SpeechEventData eventData)
+        {
+            ToggleObject(eventData.RecognizedText);
+        }
+
+    }
+}
