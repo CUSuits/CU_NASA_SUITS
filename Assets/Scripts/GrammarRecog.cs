bb2ee2e7de1efd8fb2317d71128e8190549280be
jchin <justinchin.bcchs@gmail.com>
2018-05-08 22:27:13 -0600

    added GrammarRecognition to 1_Main_current_Text2Speech scene

---
+++ Assets/Scripts/GrammarRecog.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using HoloToolkit.Unity;
+
+public class GrammarRecog : MonoBehaviour {
+    private GrammarRecognizer grammarRecognizer;
+    public TextToSpeech textToSpeechService;
+
+    // Use this for initialization
+    void Start () {
+
+        grammarRecognizer = new GrammarRecognizer(Application.streamingAssetsPath + "/SRGS/grammar.xml");
+
+        grammarRecognizer.OnPhraseRecognized += Grammar_OnPhraseRecognized;
+        grammarRecognizer.Start();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void Grammar_OnPhraseRecognized(PhraseRecognizedEventArgs args) {
+        SemanticMeaning[] meanings = args.semanticMeanings;
+        foreach (SemanticMeaning sm in meanings) {
+            Debug.Log(sm.key);
+            foreach (string value in sm.values) {
+                Debug.Log(value);
+                textToSpeechService.StartSpeaking(value);
+            }
+                
+        }
+    }
+}
