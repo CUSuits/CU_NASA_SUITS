11d32ad4b0de9a934ee9205f330eba9543b5fb18
jchin <justinchin.bcchs@gmail.com>
2018-05-11 08:06:19 -0600

    added Menu Stat Manager that holds dictionary <string,MenuStat scriptable object>

---
+++ Assets/Scripts/Menu System/Menu Statistics/MenuStatManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class StringMenuStatDict : SerializableDictionary<string, MenuStat> { }
+
+
+[Serializable]
+[CreateAssetMenu(menuName = "Menu System/Create MenuStat Manager")]
+public class MenuStatManager : ScriptableObject {
+
+    [SerializeField]
+    [Tooltip("Drag in menu stats")]
+    public StringMenuStatDict subMenuDictionary;
+
+}
