dd1ffcdc1400f2a486729421aea86a49bed44639
Anderson_PC2 <anderson_bioastro@outlook.com>
2019-04-14 17:21:57 -0600

    trying something else
    work

---
+++ 2019/Assets/Scripts/2019/ActivateListener.cs
@@ -0,0 +1,41 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateListener : MonoBehaviour {
+    public GameObject[] gmeObj;
+    public Canvas cnvs;
+    public SpeechInputHandler speechInputHandler;
+	// Use this for initialization
+	void Start () {
+        gmeObj = UnityEngine.Object.FindObjectsOfType<GameObject>();
+      
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+    public void ActivateSpeech()
+    {
+        
+        
+            foreach (GameObject cs in gmeObj)
+            {
+                speechInputHandler = cs.GetComponent<SpeechInputHandler>();
+                speechInputHandler.enabled = true;
+            }
+        
+       
+    }
+    public void StopSpeech()
+    {
+         foreach (GameObject cs in gmeObj)
+            {
+                speechInputHandler = cs.GetComponent<SpeechInputHandler>();
+                speechInputHandler.enabled = false;
+            }
+        
+    }
+}
--- Assets/Scripts/Instruction Class Files/IUpdateText.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-public interface IUpdateText {
-    void UpdateText(string newText);       
-}
